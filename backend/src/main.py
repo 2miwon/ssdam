051f0cec9e2ff0c66949925ee0e90c72833f1f9e
2miwon <zhengsfsf@gmail.com>
2025-08-21 19:10:29 +0900

    chore: deploy

--- backend/src/main.py
+++ backend/src/main.py
@@ -55,9 +55,9 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 origins = [
-    "https://ssdam.ink",
-    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8001",
     "http://localhost:3000",
+    "https://ssdam.vercel.app/"
 ]
 
 app.add_middleware(
